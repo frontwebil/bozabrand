dea5973aab183f57fb851e2aa1313a7cc48ddcf2
iLyhaha <gollfpd@gmail.com>
2026-03-24 20:32:16 +0200

    update

---
+++ src/Components/Header/Header.tsx
@@ -0,0 +1,49 @@
+import Image from "next/image";
+import Link from "next/link";
+import "./Header.css";
+
+export function Header() {
+  return (
+    <header className="header">
+      <div className="container">
+        <div className="header-logo">
+          <Image
+            src={"/logo.svg"}
+            width={260}
+            height={33}
+            alt="BOZABRAND"
+            loading="eager"
+          />
+        </div>
+        <div className="header-right">
+          <nav className="header-nav">
+            <Link href={"/"} className="header-nav-link">
+              ПРОЕКТИ
+            </Link>
+            <Link href={"/"} className="header-nav-link">
+              НЕПОВЕРхНЕВІ
+            </Link>
+            <Link href={"/"} className="header-nav-link">
+              ПРО БРЕНД
+            </Link>
+            <Link href={"/"} className="header-nav-link">
+              ПРАЙС&METОД
+            </Link>
+          </nav>
+          <div className="header-right-buttons">
+            <Link href={"tel:+380666899857"} className="header-cta-button">
+              ПОДЗВОНИТИ
+            </Link>
+            <Link
+              target="_blank"
+              href={"https://forms.gle/CeaeWGDsjqcDKhTr6"}
+              className="header-cta-button"
+            >
+              ЗАПОВНИТИ БРИФ
+            </Link>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+}

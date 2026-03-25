53c7f78a17f62dbc8cef5fa4585a82f25cb374f0
iLyhaha <gollfpd@gmail.com>
2026-03-25 14:25:26 +0200

    fix round

---
+++ src/Components/VideoInfoGraph/VideoInfoGraph.tsx
@@ -0,0 +1,16 @@
+import "./VideoInfoGraph.css";
+
+export function VideoInfoGraph() {
+  return <section className="video-info-graph">
+    <div className="container">
+      <div className="video-info-graph-cards">
+        <div className="video-info-graph-card">
+          <div className="video-info-graph-card-top">
+            <h2></h2>
+            <h2></h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>;
+}

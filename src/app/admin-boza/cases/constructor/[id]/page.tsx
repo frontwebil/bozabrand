dfb876d722fd94afd6a94870b3cbe5a845876837
Illya Sukhomlyn <gollpfd@gmail.com>
2026-04-06 02:03:58 +0300

    update

--- src/app/admin-boza/cases/constructor/[id]/page.tsx
+++ src/app/admin-boza/cases/constructor/[id]/page.tsx
@@ -1,3 +1,30 @@
-export default function Page() {
-  return <div>page</div>;
+import { prisma } from "@/lib/prisma";
+import { notFound } from "next/navigation";
+import { ConstructorPage } from "./ConstructorPage";
+
+export default async function CaseConstructorPage({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
+
+  const caseItem = await prisma.case.findUnique({
+    where: {
+      id: Number(id),
+    },
+    include: {
+      blocks: {
+        orderBy: {
+          order: "asc",
+        },
+      },
+    },
+  });
+
+  if (!caseItem) {
+    return notFound();
+  }
+
+  return <ConstructorPage caseItem={caseItem} />;
 }
